783df673d4847a7924206c6df07837544d3a7a7a
Danny Yoo <dyoo@cs.wpi.edu>
2011-08-18 15:20:42 -0400

    info.rkt fixes

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang setup/infotab
-(define name "browser-evaluate: evaluate JavaScript expressions in the browser through Racket")
+(define name "browser-evaluate")
 (define categories '(devtools))
 (define can-be-loaded-with 'all)
 (define required-core-version "5.1.2")
@@ -7,5 +7,7 @@
 (define repositories '("4.x"))
 (define scribblings '(("manual.scrbl" ())))
 (define primary-file "main.rkt")
+(define blurb 
+  '((p "browser-evaluate: evaluate JavaScript expressions in the browser through Racket.")))
 (define release-notes
   '((p "Initial release.")))
