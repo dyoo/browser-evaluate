16c481c6d6bac08e570a24e51305ef8dfb78df39
Danny Yoo <dyoo@cs.wpi.edu>
2011-08-18 15:18:27 -0400

    info.rkt

---
+++ info.rkt
@@ -0,0 +1,11 @@
+#lang setup/infotab
+(define name "browser-evaluate: evaluate JavaScript expressions in the browser through Racket")
+(define categories '(devtools))
+(define can-be-loaded-with 'all)
+(define required-core-version "5.1.2")
+(define version "1.0")
+(define repositories '("4.x"))
+(define scribblings '(("manual.scrbl" ())))
+(define primary-file "main.rkt")
+(define release-notes
+  '((p "Initial release.")))
